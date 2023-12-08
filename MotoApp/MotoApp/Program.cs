854534f78a0079d3c35450e1e4fc1d35f5ddf421
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-12-08 13:27:25 +0100

    Delete BasicStack class, add model Employee class

--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,17 +1,1 @@
-﻿using MotoApp;
-
-var stack = new BasicStack<double>();
-stack.Push(4.7);
-stack.Push(47);
-stack.Push(43.67);
-
-double sum = 0;
-
-while (stack.Count > 0)
-{
-    double item = stack.Pop();
-    Console.WriteLine($"Item: {item}");
-    sum += item;
-}
-
-Console.WriteLine(sum);
+﻿
