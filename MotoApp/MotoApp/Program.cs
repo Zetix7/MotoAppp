847735e8297313ae04c28641ad695215029d75aa
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-12-08 13:39:19 +0100

    Add 3 employees in employee repository and save (show) result

--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using MotoApp.Entities;
+using MotoApp.Repositories;
+
+var employeeRepository = new EmployeeRepository();
+employeeRepository.Add(new Employee { FirstName = "Greg" });
+employeeRepository.Add(new Employee { FirstName = "Liz" });
+employeeRepository.Add(new Employee { FirstName = "Chris" });
+employeeRepository.Save();
