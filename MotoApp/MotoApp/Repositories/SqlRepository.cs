ef043f82b631477dbb76ed14941d41cf086a504d
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-12-09 14:21:05 +0100

    Refactoring SqlRepository class

--- MotoApp/MotoApp/Repositories/SqlRepository.cs
+++ MotoApp/MotoApp/Repositories/SqlRepository.cs
@@ -9,13 +9,13 @@ public class SqlRepository<T> : IRepository<T> where T : class, IEntity, new()
 {
     private readonly DbSet<T> _dbSet;
     private readonly DbContext _dbContext;
-    private readonly ItemAddedDelegate _itemAddedDelegate;
+    private readonly ItemAddedDelegate? _itemAddedCallback;
 
-    public SqlRepository(DbContext dbContext, ItemAddedDelegate itemAddedDelegate = null)
+    public SqlRepository(DbContext dbContext, ItemAddedDelegate? itemAddedCallback = null)
     {
         _dbContext = dbContext;
         _dbSet = _dbContext.Set<T>();
-        _itemAddedDelegate = itemAddedDelegate;
+        _itemAddedCallback = itemAddedCallback;
 
     }
 
@@ -32,7 +32,7 @@ public class SqlRepository<T> : IRepository<T> where T : class, IEntity, new()
     public void Add(T item)
     {
         _dbSet.Add(item);
-        _itemAddedDelegate?.Invoke(item);
+        _itemAddedCallback?.Invoke(item);
     }
 
     public void Remove(T item)
