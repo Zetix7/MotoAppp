47af45a8b5662a176951d4a81c4d9ba16a39d1ba
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-12-11 17:13:08 +0100

    Dependency Injection in action

---
+++ MotoApp/MotoApp/App.cs
@@ -0,0 +1,34 @@
+﻿using MotoApp.Entities;
+using MotoApp.Repositories;
+using MotoApp.Repositories.Extensions;
+
+namespace MotoApp;
+
+public class App : IApp
+{
+    private readonly IRepository<Employee> _employeeRepository;
+
+    public App(IRepository<Employee> employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("I am in Run() method");
+
+        var employees = new[]
+        {
+            new Employee { FirstName="Greg"},
+            new Employee { FirstName="Liz"},
+            new Employee { FirstName="Scarlett"}
+        };
+
+        _employeeRepository.AddBatch(employees);
+
+        foreach (var employee in _employeeRepository.GetAll())
+        {
+            Console.WriteLine(employee);
+        }
+    }
+}
--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,111 +1,123 @@
-﻿using MotoApp.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using MotoApp;
 using MotoApp.Entities;
 using MotoApp.Repositories;
-using MotoApp.Repositories.Extensions;
 
-var repository = new SqlRepository<Employee>(new MotoAppDbContext());
-repository.ItemAdded += EmployeeRepositoryOnItemAdded;
-repository.ItemRemoved += EmployeeRepositoryOnItemRemoved;
+var services = new ServiceCollection();
+services.AddSingleton<IApp, App>();
+services.AddSingleton<IRepository<Employee>, ListRepository<Employee>>();
 
-Console.WriteLine("Welcome in Employees Mangement App\n");
+var serviceProvider = services.BuildServiceProvider();
+var app = serviceProvider.GetService<IApp>()!;
+app.Run();
 
-do
-{
-    Console.WriteLine("-------------------------------------------------------------------");
-    Console.WriteLine("You can READ or ADD employees from/to file.");
-    Console.WriteLine("\t1 - Read employees from file");
-    Console.WriteLine("\t2 - Add employees to file");
-    Console.WriteLine("\tQ - Exit");
-    Console.Write("Your choise: ");
-    var choise = Console.ReadLine();
+//using MotoApp.Data;
+//using MotoApp.Entities;
+//using MotoApp.Repositories;
 
-    if (choise == "q" || choise == "Q")
-    {
-        break;
-    }
-    else if (choise == "1")
-    {
-        try
-        {
-            var data = new DataInFile<Employee>(repository);
-            data.ItemRead += EmployeesFileOnItemRead;
-            data.Read();
-            WriteAllToConsole(repository);
-        }
-        catch (FileNotFoundException fe)
-        {
-            Console.WriteLine($"Exception catched: {fe.Message}");
-        }
-        catch (FileLoadException fe)
-        {
-            Console.WriteLine($"Exception catched: {fe.Message}");
-        }
-    }
-    else if (choise == "2")
-    {
-        try
-        {
-            Console.Write("Insert employee name: ");
-            var employeeName = Console.ReadLine();
-            var data = new DataInFile<Employee>(repository);
-            data.ItemAdded += EmployeeFileOnItemAdded;
-            data.Add(new Employee { FirstName = employeeName });
-        }
-        catch (FileNotFoundException fe)
-        {
-            Console.WriteLine($"Exception catched: {fe.Message}");
-        }
-        catch (ArgumentOutOfRangeException ae)
-        {
-            Console.WriteLine($"Exception catched: {ae.Message}");
-        }
-        catch(NullReferenceException ne)
-        {
-            Console.WriteLine($"Exception catched: {ne.Message}");
-        }
-    }
-    else
-    {
-        Console.WriteLine("\tTry again!");
-    }
-} while (true);
+//var repository = new SqlRepository<Employee>(new MotoAppDbContext());
+//repository.ItemAdded += EmployeeRepositoryOnItemAdded;
+//repository.ItemRemoved += EmployeeRepositoryOnItemRemoved;
 
-static void FillAuditFile(string action, string item)
-{
-    using(var writer = File.AppendText("audit.txt"))
-    {
-        writer.WriteLine($"[{DateTime.Now}]-{action,-29}-[{item}]");
-    }
-}
+//Console.WriteLine("Welcome in Employees Mangement App\n");
 
-static void EmployeeFileOnItemAdded(object? sender, Employee e)
-{
-    Console.WriteLine($"\tAdded to file. Employee: {e.FirstName} execute from {sender?.GetType().Name}");
-    FillAuditFile("EmployeeAddedToFile", e.FirstName!);
-}
+//do
+//{
+//    Console.WriteLine("-------------------------------------------------------------------");
+//    Console.WriteLine("You can READ or ADD employees from/to file.");
+//    Console.WriteLine("\t1 - Read employees from file");
+//    Console.WriteLine("\t2 - Add employees to file");
+//    Console.WriteLine("\tQ - Exit");
+//    Console.Write("Your choise: ");
+//    var choise = Console.ReadLine();
 
-static void EmployeesFileOnItemRead(object? sender, Employee e)
-{
-    Console.WriteLine($"\tRead from file. Employee: {e.FirstName} execute from {sender?.GetType().Name}");
-    FillAuditFile("EmployeeReadFromFile", e.FirstName!);
-}
+//    if (choise == "q" || choise == "Q")
+//    {
+//        break;
+//    }
+//    else if (choise == "1")
+//    {
+//        try
+//        {
+//            var data = new DataInFile<Employee>(repository);
+//            data.ItemRead += EmployeesFileOnItemRead;
+//            data.Read();
+//            WriteAllToConsole(repository);
+//        }
+//        catch (FileNotFoundException fe)
+//        {
+//            Console.WriteLine($"Exception catched: {fe.Message}");
+//        }
+//        catch (FileLoadException fe)
+//        {
+//            Console.WriteLine($"Exception catched: {fe.Message}");
+//        }
+//    }
+//    else if (choise == "2")
+//    {
+//        try
+//        {
+//            Console.Write("Insert employee name: ");
+//            var employeeName = Console.ReadLine();
+//            var data = new DataInFile<Employee>(repository);
+//            data.ItemAdded += EmployeeFileOnItemAdded;
+//            data.Add(new Employee { FirstName = employeeName });
+//        }
+//        catch (FileNotFoundException fe)
+//        {
+//            Console.WriteLine($"Exception catched: {fe.Message}");
+//        }
+//        catch (ArgumentOutOfRangeException ae)
+//        {
+//            Console.WriteLine($"Exception catched: {ae.Message}");
+//        }
+//        catch(NullReferenceException ne)
+//        {
+//            Console.WriteLine($"Exception catched: {ne.Message}");
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine("\tTry again!");
+//    }
+//} while (true);
 
-static void EmployeeRepositoryOnItemAdded(object? sender, Employee e)
-{
-    Console.WriteLine($"\tAdded to Repository. Employee: {e.FirstName} execute form {sender?.GetType().Name}");
-    FillAuditFile("EmployeeAddedToRepository", e.FirstName!);
-}
+//static void FillAuditFile(string action, string item)
+//{
+//    using(var writer = File.AppendText("audit.txt"))
+//    {
+//        writer.WriteLine($"[{DateTime.Now}]-{action,-29}-[{item}]");
+//    }
+//}
 
-static void EmployeeRepositoryOnItemRemoved(object? sender, Employee e)
-{
-    Console.WriteLine($"\tRemoved from Repository. Employee: {e.FirstName} execute form {sender?.GetType().Name}");
-    FillAuditFile("EmployeeRemovedFromRepository", e.FirstName!);
-}
+//static void EmployeeFileOnItemAdded(object? sender, Employee e)
+//{
+//    Console.WriteLine($"\tAdded to file. Employee: {e.FirstName} execute from {sender?.GetType().Name}");
+//    FillAuditFile("EmployeeAddedToFile", e.FirstName!);
+//}
 
-static void WriteAllToConsole(IReadRepository<IEntity> repository)
-{
-    foreach (var item in repository.GetAll())
-    {
-        Console.WriteLine(item);
-    }
-}
+//static void EmployeesFileOnItemRead(object? sender, Employee e)
+//{
+//    Console.WriteLine($"\tRead from file. Employee: {e.FirstName} execute from {sender?.GetType().Name}");
+//    FillAuditFile("EmployeeReadFromFile", e.FirstName!);
+//}
+
+//static void EmployeeRepositoryOnItemAdded(object? sender, Employee e)
+//{
+//    Console.WriteLine($"\tAdded to Repository. Employee: {e.FirstName} execute form {sender?.GetType().Name}");
+//    FillAuditFile("EmployeeAddedToRepository", e.FirstName!);
+//}
+
+//static void EmployeeRepositoryOnItemRemoved(object? sender, Employee e)
+//{
+//    Console.WriteLine($"\tRemoved from Repository. Employee: {e.FirstName} execute form {sender?.GetType().Name}");
+//    FillAuditFile("EmployeeRemovedFromRepository", e.FirstName!);
+//}
+
+//static void WriteAllToConsole(IReadRepository<IEntity> repository)
+//{
+//    foreach (var item in repository.GetAll())
+//    {
+//        Console.WriteLine(item);
+//    }
+//}
