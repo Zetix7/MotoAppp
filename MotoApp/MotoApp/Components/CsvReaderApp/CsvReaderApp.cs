aabc4dd5a1082ec935834f7288b6fc6f867a94ae
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-12-12 21:56:44 +0100

    Created manufacturersFuel.xml file in CsvReaderApp class

--- MotoApp/MotoApp/Components/CsvReaderApp/CsvReaderApp.cs
+++ MotoApp/MotoApp/Components/CsvReaderApp/CsvReaderApp.cs
@@ -1,4 +1,6 @@
 ﻿using MotoApp.Components.CsvReader;
+using MotoApp.Components.CsvReader.Models;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace MotoApp.Components.CsvReaderApp;
@@ -26,6 +28,7 @@ internal class CsvReaderApp : ICsvReaderApp
             Console.WriteLine("\t3 - GroupJoin");
             Console.WriteLine("\t4 - Create fuel.xml file");
             Console.WriteLine("\t5 - Read fuel.xml file");
+            Console.WriteLine("\t6 - Print datas and create manufacturersFuel.xml file");
             Console.WriteLine("\tQ - Exit");
             Console.Write("\t\tYour choise: ");
             var choise = Console.ReadLine();
@@ -121,9 +124,46 @@ internal class CsvReaderApp : ICsvReaderApp
                     Console.WriteLine(manufacturerXml);
                 }
             }
+            else if (choise == "6")
+            {
+                var group = manufacturers.GroupJoin(
+                    cars,
+                    x => x.Name,
+                    x => x.Manufacturer,
+                    (manufactuerer, cars) => new
+                    {
+                        Manufacturer = manufactuerer,
+                        Cars = cars
+                    });
+
+                foreach (var g in group)
+                {
+                    Console.WriteLine($"Manufacturer - {g.Manufacturer.Name}, Country: {g.Manufacturer.Country}");
+                    Console.WriteLine($"\tCars - CombinedSum: {g.Cars.Sum(x => x.Combined)}");
+                    foreach (var car in g.Cars)
+                    {
+                        Console.WriteLine($"\t\tCar - {car.Name} Combined: {car.Combined}");
+                    }
+                }
+
+                var document = new XDocument();
+                var manufacturersXml = new XElement("Manufacturers", group
+                    .Select(x =>
+                        new XElement("Manufacturer",
+                            new XAttribute("Name", x.Manufacturer.Name!),
+                            new XAttribute("Country", x.Manufacturer.Country!),
+                            new XElement("Cars", x.Cars.Select(x =>
+                                new XElement("Car",
+                                    new XAttribute("Model", x.Name!),
+                                    new XAttribute("Combined", x.Combined))),
+                                new XAttribute("Country", x.Manufacturer.Country!),
+                                new XAttribute("CombinedSum", x.Cars.Sum(x => x.Combined)!)))));
+                document.Add(manufacturersXml);
+                document.Save("Resources//Files//manufacturersFuel.xml");
+            }
             else
             {
-                Console.WriteLine("Choose 1 or 2 or 3 or 4 or 5 or Q! No more options!");
+                Console.WriteLine("Choose 1 or 2 or 3 or 4 or 5 or 6 or Q! No more options!");
                 Console.WriteLine("\tIf you do not choose, You will stuck here forever!");
             }
         } while (true);
