6509c59d28e9607208ed130d15fd75b646739ae2
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-12-16 15:35:58 +0100

    Refactoring CsvReader class

--- MotoApp/MotoApp/Components/CsvReader/CsvReader.cs
+++ MotoApp/MotoApp/Components/CsvReader/CsvReader.cs
@@ -15,18 +15,6 @@ namespace MotoApp.Components.CsvReader
 
             var cars = File.ReadAllLines(filePath)
                 .Skip(1).Where(x => x.Length > 1).ToCar().ToList();
-            //.Select(x => x.Split(','))
-            //.Select(x => new Car
-            //{
-            //    Year = int.Parse(x[0], CultureInfo.InvariantCulture),
-            //    Manufacturer = x[1],
-            //    Name = x[2],
-            //    Displacement = double.Parse(x[3], CultureInfo.InvariantCulture),
-            //    Cylinders = int.Parse(x[4], CultureInfo.InvariantCulture),
-            //    City = int.Parse(x[5], CultureInfo.InvariantCulture),
-            //    Highway = int.Parse(x[6], CultureInfo.InvariantCulture),
-            //    Combined = int.Parse(x[7], CultureInfo.InvariantCulture),
-            //}).ToList();
             return cars;
         }
 
